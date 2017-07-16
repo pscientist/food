620875798fca1a5641f906db7087c06684d5179c
Cathy Lai <cathy@fishpond.co.nz>
2017-07-17 08:08:37 +1200

    First commit

---
+++ polls/crashCourse.py
@@ -0,0 +1,19 @@
+
+# comments
+print("double quotes")
+
+print('single quotes')
+
+print('''Multiline comments''')
+
+# word slice
+print('Hello'[0]) # prints H
+
+print('Hello'[0-2]) # prints Hel
+
+print(2) # prints 2
+
+print('\n') # prints new line
+
+
+# variables and data types
